975c3a92ed6b8cb55ee981ed3c51ef9253fa3167
Sabi <128085045+7grandlittlekids@users.noreply.github.com>
2025-02-23 16:47:38 -0600

    Create NewMainScript.lua

---
+++ NewMainScript.lua
@@ -0,0 +1,8 @@
+if(game.PlaceId == 118688242561353) then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/7grandlittlekids/OTTDOG/refs/heads/main/Lobby.lua"))()
+else
+if(game.PlaceId == 90192874177359) then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/7grandlittlekids/OTTDOG/refs/heads/main/Nightmare.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/7grandlittlekids/OTTDOG/refs/heads/main/Skip.lua"))()
+end
+end
