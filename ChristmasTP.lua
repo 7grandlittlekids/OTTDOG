e0a3eec57ab78bdb15f297065940a8b80a1dae56
Sabi <128085045+7grandlittlekids@users.noreply.github.com>
2025-02-28 15:22:01 -0600

    Update ChristmasTP.lua

--- ChristmasTP.lua
+++ ChristmasTP.lua
@@ -1,23 +1,23 @@
 if(game.PlaceId == 118688242561353) then
 local function teleportToPosition()
     -- วาร์ปตัวละครไปยังตำแหน่งที่กำหนด
-    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-280.076996, 633.415466, 755.718994, 0.654158831, -6.47220872e-08, 0.756357193, 1.08048937e-07, 1, -7.87865773e-09, -0.756357193, 8.68774848e-08, 0.654158831)
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-280.405029, 633.415466, 724.407288, -0.0104775475, 1.09676769e-07, 0.999945104, 8.43243608e-09, 1, -1.09594438e-07, -0.999945104, 7.28369232e-09, -0.0104775475)    
 end
 
 local function handleElevator()
-    local statusText = workspace.Elevator8.Screen.StatusGui.Title.Text
+    local statusText = workspace.Elevator21.Screen.StatusGui.Title.Text
 
     if statusText == "2/4" then
         -- ส่งคำสั่งไปยังเซิร์ฟเวอร์
         local args = {
-            [1] = 8
+            [1] = 543
         }
         game:GetService("ReplicatedStorage").Elevator:FireServer(unpack(args))
 
         -- รอจนกว่าสถานะจะเปลี่ยนเป็น "0/4"
         repeat
             wait(0.1) -- รอเล็กน้อยเพื่อหลีกเลี่ยงการใช้ทรัพยากรมากเกินไป
-            statusText = workspace.Elevator8.Screen.StatusGui.Title.Text
+            statusText = workspace.Elevator21.Screen.StatusGui.Title.Text
         until statusText == "0/4"
 
         -- วาร์ปตัวละครไปยังตำแหน่งเดิม
