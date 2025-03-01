5d83ce4f0b708ca06acdbdeb607e9fb11c4dd12e
Sabi <128085045+7grandlittlekids@users.noreply.github.com>
2025-03-01 00:47:37 -0600

    Update AutoReplayChristmas.lua

--- AutoReplayChristmas.lua
+++ AutoReplayChristmas.lua
@@ -1,1 +1,23 @@
+local function Replay()
+local args = {}
 
+game:GetService("ReplicatedStorage").Events.ExitGame:FireServer()
+warn("REPLAYING")
+end
+
+local EndScreen = game.Players.LocalPlayer.PlayerGui.GameGui.EndScreen.Visible
+
+if EndScreen == false then
+warn("Game hasnt ended!")
+
+   repeat
+   wait(0.5)
+   
+   local EndScreen = game.Players.LocalPlayer.PlayerGui.GameGui.EndScreen.Visible
+   
+   until EndScreen == true
+   
+   Replay()
+end
+
+Replay()
