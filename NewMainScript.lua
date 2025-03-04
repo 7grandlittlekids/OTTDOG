2b017ff4ce22ecc3ce8bda541d4aac35b9b4954e
Sabi <128085045+7grandlittlekids@users.noreply.github.com>
2025-03-04 16:34:37 -0600

    Delete NewMainScript.lua

--- NewMainScript.lua
+++
@@ -1,7 +0,0 @@
-if(game.PlaceId == 118688242561353) then
-loadstring(game:HttpGet("https://raw.githubusercontent.com/7grandlittlekids/OTTDOG/refs/heads/main/Lobby.lua"))()
-else
-if(game.PlaceId == 90192874177359) then
-loadstring(game:HttpGet("https://raw.githubusercontent.com/7grandlittlekids/OTTDOG/refs/heads/main/Nightmare.lua"))()
-end
-end
