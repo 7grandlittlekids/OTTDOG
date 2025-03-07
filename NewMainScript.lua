5dc8f1979f863755ab88b4bc8e5124ee35d8e0d8
Sabi <128085045+7grandlittlekids@users.noreply.github.com>
2025-03-07 16:53:44 -0600

    Create NewMainScript.lua

---
+++ NewMainScript.lua
@@ -0,0 +1,2 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/7grandlittlekids/OTTDOG/refs/heads/main/Nightmare.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/7grandlittlekids/OTTDOG/refs/heads/main/Lobby.lua"))()
